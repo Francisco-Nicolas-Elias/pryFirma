2baa2a08e6d6c90414cc804bb8e9c7025c1b7a4a
Francisco Elias <franeliasbv@gmail.com>
2024-03-25 20:06:15 -0300

    Se realiza el dibujo de la firma pero no logro guardarla

--- Form1.cs
+++ Form1.cs
@@ -35,7 +35,7 @@ namespace pryFirma
         //Trabajo sobre el evento en el que el usuario hace click sobre el PictureBox
         private void pbFirma_MouseDown(object sender, MouseEventArgs e)
         {
-            //Al poner la variable en true indíco que el usuario esta dibujando sobre el PictureBox
+            //Al poner la variable en true indico que el usuario esta dibujando sobre el PictureBox
             dibujando = true;
 
             //Se actualiza con la posición del mouse en el momento que se hace click
@@ -46,8 +46,10 @@ namespace pryFirma
         //Trabajo sobre el evento en el que el usuario mueve el mouse con el click presionado
         private void pbFirma_MouseMove(object sender, MouseEventArgs e)
         {
+            //Si está dibujando el usuario ingresa al if 
             if (dibujando)
             {
+                //Con el objeto Graphics del PictureBox dibuja una línea entre la posición anterior y la actual del cursor
                 using (Graphics g = pbFirma.CreateGraphics())
                 {
                     //Dibuja una línea desde la posición anterior a la posición actual
@@ -58,6 +60,7 @@ namespace pryFirma
             }
         }
 
+        //En este evento pongo en false la variable booleana para indicar que el usuario dejó de dibujar
         private void pbFirma_MouseUp(object sender, MouseEventArgs e)
         {
             dibujando = false;
@@ -65,9 +68,10 @@ namespace pryFirma
 
         private void btnGuardarFirma_Click(object sender, EventArgs e)
         {
+            //Verifico que la Propiedad Image del PictureBox no sea nula, en ese caso ingreso al if
             if (pbFirma.Image != null)
             {
-                //Guarda la firma en una imagen PNG
+                //Guarda la firma en una imagen PNG, lo hago con el método Save del objeto Image
                 pbFirma.Image.Save("firma.png", System.Drawing.Imaging.ImageFormat.Png);
                 MessageBox.Show("Firma guardada correctamente.");
             }
